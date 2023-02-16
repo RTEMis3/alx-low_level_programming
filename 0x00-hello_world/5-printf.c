33fab72d58becaf893c540db2347e11366e0d34c
Tomisin <tomisinolorunfemi@gmail.com>
2023-02-16 15:14:30 -0800

    DONE

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - This prints a line using printf function
+ * return: Returns to 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
