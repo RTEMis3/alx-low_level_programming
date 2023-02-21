6eecfb013f28cd8a8211873af6766db4d535c2ae
Tomisin <tomisinolorunfemi@gmail.com>
2023-02-21 12:02:46 -0800

    DONE

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+/**
+ * main - Entry point
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: Always zero (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	putchar("\"_putchar")
+	}
+	putchar('\n');
+	return (0);
+}
