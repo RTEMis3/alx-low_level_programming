47970a80ceac7d08e292b8126ee938fd897cfbc2
Tomisin <tomisinolorunfemi@gmail.com>
2023-02-22 02:20:30 -0800

    DONE

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 9)
+	{
+	char c;
+
+	for (c = 'a'; c <= 'z' ; c++);
+	}
+	i++;
+}
