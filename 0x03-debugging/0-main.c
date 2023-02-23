d7f6cf0c279089e693dd8a992f7082f1290d82e2
Tomisin <tomisinolorunfemi@gmail.com>
2023-02-23 09:32:08 -0800

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include "main.h"
 
 /** 
  * main - Check if a number.
@@ -11,21 +12,10 @@
  */
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-	printf("%d is positive\n", n);
-	}
-	else if (n < 0)
-	{
-	printf("%d is negative\n", n);
-	}
-	else
-	{
-	printf("%d is zero\n", n);
-	}
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
