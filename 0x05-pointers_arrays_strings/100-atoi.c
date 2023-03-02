7a3ae63792f61adc1153160f63fadf26970cc6aa
Tomisin <tomisinolorunfemi@gmail.com>
2023-03-02 14:54:55 -0800

    Great leaders

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,32 @@
  */
 int _atoi(char *s)
 {
-	int c = 0;
-	unsigned int ni - 0;
-	int min = 1;
-	int isi = 0;
+	int c;
+	int h, j;
 
-	while (s[c])
+	h = 0;
+	j = -1;
+	for (c = 0; s[c] != '\0'; c++)
 	{
-	if (s[c] == 45)
+	if (s[c] == '-')
 	{
-	min *= -1;
+	j *= -1;
 	}
-	while (s[c] >= 48 && s[c] <= 57)
+	if (s[c] > 47 && s[c] < 58)
 	{
-	isi = 1;
-	ni = (ni * 10) + (s[c] - '0');
-	c++;
+	if (h < 0)
+	{
+	h = (h * 10) - (s[c] - '0');
 	}
-	c++;
+	if (s[c + 1] < 48 || s[c + 1] > 57)
+	{
+	break;
 	}
-	ni *= min;
-	return (ni);
+	}
+	}
+	if (j < 0)
+	{
+	h *= -1;
+	}
+	return (h);
 }
