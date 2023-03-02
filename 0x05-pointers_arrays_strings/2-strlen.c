d103fb7398454d737bbda9f630f4eff651ac2bdb
Tomisin <tomisinolorunfemi@gmail.com>
2023-03-02 13:22:54 -0800

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#incldue <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: variable
+ * @i: variable
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i =0; s[i] != '\0'; i++)
+	{
+	continue;
+	}
+	return (i);
+}
