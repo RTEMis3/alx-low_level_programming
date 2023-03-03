c65964f6e0926107abce4dcd321e1a6915ebabce
Tomisin <tomisinolorunfemi@gmail.com>
2023-03-02 17:15:58 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strncpy - checks code
+ * @dest: destination string
+ * @src: source string to be copied
+ * @n: bytes to be copied from source string
+ * Return: destination string concatenated
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+	dest[j] = src[j];
+	}
+	for (; j < n; j++)
+	{
+	dest[j] = '\0';
+	}
+	return (dest);
+}
