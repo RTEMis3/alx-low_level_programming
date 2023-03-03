ca3578d63865239117d0cd549cc7feaeb319bc87
Tomisin <tomisinolorunfemi@gmail.com>
2023-03-02 17:22:49 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - checks the code
+ * @s1: destination string
+ * @s2: source string to be copied
+ * Return: difference of the first characters that are of diff values
+ */
+int _strcmp(char *s1, char *s2)
+	int j;
+
+	for (j = 0; s1[j] != '\0' && s2[j] != '\0'; j++)
+	{
+	if (s1[j] != s2[j])
+	{
+	return (s1[j] - s2[j]);
+	}
+	}
+	return (0);
+}
