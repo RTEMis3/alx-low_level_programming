c1c298d394e1b0c22213a8435eafd3b655e5e60c
Tomisin <tomisinolorunfemi@gmail.com>
2023-03-02 18:59:04 -0800

    Function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,9 +10,9 @@ char *leet(char *s)
 {
 	int i;
 	int j;
-
 	char letters[] = "aAeEoOtTlL";
-	char leet_it[] = "4433007711";	
+	char leet_it[] = "4433007711";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 	for (j = 0; j < 10; j++)
