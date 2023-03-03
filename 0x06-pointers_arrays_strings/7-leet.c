2fe47493ea71d1076da241104a75e311dccbd812
Tomisin <tomisinolorunfemi@gmail.com>
2023-03-02 18:45:20 -0800

    Function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - checks the code
+ * @s: input string to be translated
+ * Return: string after conversion of 'leet'
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char letters[] = "aEeEoOtTlL";
+	char leet_it[] = "4433007711";
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (j = 0; j < 10; j++)
+	{
+	if (s[i] == letters[j])
+	{
+	s[i] = leet_it[j];
+	}
+	}
+	}
+	return (s);
+}
