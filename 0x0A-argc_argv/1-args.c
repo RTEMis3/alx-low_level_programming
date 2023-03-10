7910da77771db34421ef3f314a3e17ce1d25fc32
Tomisin <tomisinolorunfemi@gmail.com>
2023-03-10 15:38:08 -0800

    A program that prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ * @argv: the string from the terminal
+ * @argc: the number of parameters
+ * Return: 0.
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc -1);
+	return (0);
+}
