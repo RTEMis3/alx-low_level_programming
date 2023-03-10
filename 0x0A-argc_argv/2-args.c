7e6c221175c5a66a4c902e247fd5b60d03b0821b
Tomisin <tomisinolorunfemi@gmail.com>
2023-03-10 15:47:14 -0800

    A program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @argv: the string from the terminal
+ * @argc: the number of parameters
+ * Return: 0.
+ */
+int main(int argc, char const *argv[])
+{
+	while (argc --)
+	{
+	printf("%s\n", *argv++);
+	}
+	return (0);
+}
