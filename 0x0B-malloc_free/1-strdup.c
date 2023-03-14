dc99d71c08958e1aecef33deff7546b6b1ff8e60
Tomisin <tomisinolorunfemi@gmail.com>
2023-03-14 15:19:46 -0700

    A function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,61 +1,35 @@
 #include "main.h"
 
 /**
- * _strlen - count array
- * @s: array of elements
- * Return: 1
- */
-int _strlen(char *s)
-{
-	unsigned int i;
-
-	i = 0;
-	while (s[i] != '\0') /*count character of string*/
-	{
-	i++;
-	}
-	return (i);
-}
-/**
- * _strcpy - copy arrays
- * @src: array of elements
- * @dest: dest array
- * Return: dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-	dest[i] = src[i];
-	i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-/**
- * _strdup - array for prints a string
- * @str: array of elements
- * Return: pointer
+ * _strdup - main function
+ * @str: the char that will have the size
+ * Return: character
  */
 char *_strdup(char *str)
 {
-	char *dst;
-	unsigned int size;
+	int i, j;
+	char *ret;
 
-	if (str == 0)
+	if (str == NULL)
 	{
 	return (NULL);
 	}
-	size = _strlen(str) + 1;
-	
-	dst = (char *) malloc(size *sizeof(char));
-	
-	if (dst == 0)
+	j = 0;
+	while (str[j] != 0)
+	{
+	j++;
+	}
+	ret = malloc(1 + (j *sizeof(*str)));
+	if (ret == NULL)
 	{
 	return (NULL);
 	}
-	_strcpy(dst, str);
-	return (dst);
+	else
+	{
+	for (i = 0; i < j; i++)
+	{
+	ret[i] = str[i];
+	}
+	}
+	return (ret);
 }
