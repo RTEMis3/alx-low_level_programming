3cc579763c01c97e824893905c0b5a8ec290f2e4
Tomisin <tomisinolorunfemi@gmail.com>
2023-03-16 15:19:09 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,34 +9,20 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr;
+	int *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
 	return (NULL);
 	}
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
+	p = malloc(nmemb * size);
+	if (p == NULL)
 	{
 	return (NULL);
 	}
-	_memset((void *)ptr, 0, size * nmemb);
-	return (ptr);
-}
-/**
- * _memset - fills memory with a constant byte
- * @new_ptr: the pointer
- *  @c: the char
- *  @size: the amount of memory being filled
- *  Return: the new memory
- */
-char *_memset(char *new_ptr, char c, unsigned int size)
-{
-	char *ptr = new_ptr;
-
-	while (size--)
+	for (i = 0; i < (nmemb * size); i++)
 	{
-	*ptr++ = c;
-	}
-	return (new_ptr);
+	p[i] = 0;
+	return (p);
 }
