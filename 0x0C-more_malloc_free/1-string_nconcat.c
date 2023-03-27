7950ebd58a3554262ab3d6631e8dd2c7b59940a4
Tomisin <tomisinolorunfemi@gmail.com>
2023-03-26 20:17:25 -0700

    DONE

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,36 +1,50 @@
 #include "main.h"
 
 /**
- * string_nconcat - function to concatenate strings with n bytes
- * @s1: destination for concatenation
- * @s2: source of string
- * @n: int type for size of byte
- * Return: pointer to new memory allocated
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *sout;
-	unsigned int ls1, ls2, lsout, i;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
-		s1 = "";
+	{
+	s1 = "";
+	}
 	if (s2 == NULL)
-		s2 = "";
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
-	if (n > ls2)
-		n = 1s2;
-	lsout = ls1 + n;
-	sout = malloc(lsout + 1);
-	if (sout == NULL)
-		return (NULL);
-	for (i = 0; i < lsout; i++)
-		if (i < ls1)
-			sout[i] = s1[i];
-		else
-			sout[i] = s2[i - ls1];
-	sout[i] = '\0';
-	return (sout);
+	{
+	s2 = "";
+	}
+	while (s1[size1] != '\0')
+	{
+	size1++;
+	}
+	while (s2[size2] != '\0')
+	{
+	size2++;
+	}
+	if (n > size2)
+	{
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+	}
+	if (p == NULL)
+	{
+	return (0);
+	for (i = 0; i < size1; i++)
+	{
+	p[i] = s1[i];
+	}
+	for (; i < (size1 + n); i++)
+	{
+	p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+	return (p);
 }
